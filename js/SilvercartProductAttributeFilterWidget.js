faeb309c14b7b99ea0d905a307c35bd8e712ae0c
Sebastian Diel <sdiel@pixeltricks.de>
2012-06-08 19:43:14 +0200

    ENHANCEMENT: added js reload fallback on ajax error

--- js/SilvercartProductAttributeFilterWidget.js
+++ js/SilvercartProductAttributeFilterWidget.js
@@ -56,6 +56,7 @@ var SilvercartProductAttributeFilterCall                = function() {
                     }
                 },
                 'error':    function() {
+                    document.location.reload();
                     $('.silvercart-product-attribute-filter-loading-bar').hide('slow', function() {
                         $('.silvercart-product-attribute-filter-mask').fadeOut('slow');
                     });
