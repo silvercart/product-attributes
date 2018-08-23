f44bc5f3b6587db2cf5901e4057723789ace71ea
Sebastian Diel <sdiel@pixeltricks.de>
2018-08-23 11:16:36 +0200

    MINOR-ENHANCEMENT: Updated support to the most current SilverCart version.

--- _config.php
+++ _config.php
@@ -2,9 +2,9 @@
 
 use SilverCart\Model\Pages\ProductGroupPageController;
 use SilverCart\Model\Pages\SearchResultsPageController;
-use SilverCart\Model\Widgets\WidgetController;
+use SilverCart\Model\Widgets\Widget;
 use SilverCart\ProductAttributes\Plugins\ProductFilterPlugin;
 
 ProductGroupPageController::registerFilterPlugin(ProductFilterPlugin::class);
 SearchResultsPageController::registerFilterPlugin(ProductFilterPlugin::class);
-WidgetController::registerFilterPlugin(ProductFilterPlugin::class);
+Widget::registerFilterPlugin(ProductFilterPlugin::class);
